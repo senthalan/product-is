b93cd768f2e71ee3f3d1d88f16bbbcf83211d601
senthalan <senthalank@gmail.com>
2019-08-16 13:34:51 +0530

    Building with kernel alpha

--- modules/samples/oauth2/playground2/src/main/org/wso2/sample/identity/oauth2/OIDCBackchannelLogoutServlet.java
+++ modules/samples/oauth2/playground2/src/main/org/wso2/sample/identity/oauth2/OIDCBackchannelLogoutServlet.java
@@ -20,7 +20,7 @@ package org.wso2.sample.identity.oauth2;
 import com.nimbusds.jwt.SignedJWT;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -40,7 +40,7 @@ public class OIDCBackchannelLogoutServlet extends HttpServlet {
 
     public void init(ServletConfig config) throws SecurityException {
 
-        BasicConfigurator.configure();
+//        BasicConfigurator.configure();
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
